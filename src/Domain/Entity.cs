38ed3682bac13f44a86ffe4cc5dee49392583dd3
Gabriel <gabrielsprata@hotmail.com>
2023-08-07 10:22:35 -0300

    [FEAT] Created Product Domain

---
+++ src/Domain/Entity.cs
@@ -0,0 +1,16 @@
+﻿namespace IWantApp.Domain;
+
+public abstract class Entity
+{
+    //Ao Instanciar um objeto com esta categoria o Id já é gerado automaticamente
+    public Entity()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public Guid Id { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public string EditedBy { get; set; }
+    public DateTime EditedOn { get; set; }
+}
---
+++ src/Domain/Products/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace IWantApp.Domain.Products;
+
+public class Product : Entity
+{
+    //O Guid gera uma espécie de hash para os Id's e isso torna a aplicação mais segura porém menos performática
+    public string Name { get; set; }
+    public Category Category { get; set; }
+    public string Description { get; set; }
+    public bool HasStock { get; set; }
+}
