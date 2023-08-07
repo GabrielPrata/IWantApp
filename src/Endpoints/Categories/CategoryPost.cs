790825443de8bd1e544965656bbfe15e3d8055ed
Gabriel <gabrielsprata@hotmail.com>
2023-08-07 10:55:37 -0300

    [FEAT] Created first endpoint

---
+++ src/Endpoints/Categories/CategoryPost.cs
@@ -0,0 +1,19 @@
+﻿using IWantApp.Infra.Data;
+
+namespace IWantApp.Endpoints.Categories;
+
+public class CategoryPost
+{
+    //Já atribuo um valor ao criar a propriedade
+    public static string Template => "/categories";
+
+    //Seto os métodos permitidos para esta endpoint
+    public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
+
+    public static Delegate Handle => Action;
+
+    public static IResult Action(CategoryRequest categoryRequest, ApplicationDbContext context)
+    {
+        return Results.Ok("Ok");
+    }
+}
