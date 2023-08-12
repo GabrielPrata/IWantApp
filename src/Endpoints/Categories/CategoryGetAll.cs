7f810e868907627bc93c9241d8a08c2cf63eecb1
Gabriel <gabrielsprata@hotmail.com>
2023-08-12 17:14:41 -0300

    [FIX] Fixed PUT method error

---
+++ src/Endpoints/Categories/CategoryGetAll.cs
@@ -0,0 +1,26 @@
+﻿using IWantApp.Domain.Products;
+using IWantApp.Infra.Data;
+
+namespace IWantApp.Endpoints.Categories;
+
+public class CategoryGetAll
+{
+    //Já atribuo um valor ao criar a propriedade
+    public static string Template => "/categories";
+
+    //Seto os métodos permitidos para esta endpoint
+    public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
+
+    public static Delegate Handle => Action;
+
+    public static IResult Action(ApplicationDbContext context)
+    {
+        var categories = context.Categories.ToList();
+
+        //Aqui, eu passo como category response para que as informações de log (Edited/Created by e on)
+        //não sejam exibidas para os usuários
+        var response = categories.Select(c => new CategoryResponse { Id = c.Id, Name = c.Name, Active = c.Active });
+
+        return Results.Ok(response);
+    }
+}
