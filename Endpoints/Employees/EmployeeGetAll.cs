310e3352e19e7662a9c3b2358be9d92ee1182858
Gabriel <gabrielsprata@hotmail.com>
2023-12-13 09:55:41 -0300

    [Feat] Added method GET /employees

---
+++ Endpoints/Employees/EmployeeGetAll.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace IWantApp.Endpoints.Employees;
+
+public class EmployeeGetAll
+{
+    //Já atribuo um valor ao criar a propriedade
+    public static string Template => "/employees";
+
+    //Seto os métodos permitidos para esta endpoint
+    public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
+
+    public static Delegate Handle => Action;
+
+    //UserManager gerencia IdentityUser
+    public static IResult Action(UserManager<IdentityUser> userManager)
+    {
+        var users = userManager.Users.ToList();
+
+        var employees = new List<EmployeeResponse>();
+        foreach (var user in users)
+        {
+            var claims = userManager.GetClaimsAsync(user).Result;
+            var claimName = claims.FirstOrDefault(c => c.Type == "Name");
+            var userName = claimName != null ? claimName.Value : string.Empty;
+
+            employees.Add(new EmployeeResponse(user.Email, userName));
+        }
+
+        return Results.Ok(employees);
+    }
+}
---
+++ Endpoints/Employees/EmployeeResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace IWantApp.Endpoints.Employees;
+
+public record EmployeeResponse(string Email, string Name)
+{
+
+}
